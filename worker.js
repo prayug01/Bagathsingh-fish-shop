0b895c80ff154d4ccef501481027ed4d9d75a09b
bharath2 <sbarat796@gmail.com>
2026-03-11 14:04:41 +0530

    chnages done

--- worker.js
+++ worker.js
@@ -24,6 +24,17 @@ self.addEventListener('install', (event) => {
   );
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
