79c83a513c1492be3d3a77e2ee1d4eb9fab87f06
bharath2 <sbarat796@gmail.com>
2026-03-11 14:04:22 +0530

    more menu have been added

--- worker.js
+++ worker.js
@@ -1,12 +1,18 @@
 // Service Worker for Bagathsingh Fish Shop
-const CACHE_NAME = 'bagathsingh-fish-v1';
+const CACHE_NAME = 'bagathsingh-fish-v2';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/style.css',
   '/assets/hero.png',
   '/assets/scales.png',
-  '/assets/category_fresh.png'
+  '/assets/category_fresh.png',
+  '/assets/logo.jpg',
+  '/assets/nagarai.jpg',
+  '/assets/thirukkai.jpg',
+  '/assets/vilai.jpg',
+  '/assets/squid.jpg',
+  '/assets/prawns.png'
 ];
 
 self.addEventListener('install', (event) => {
